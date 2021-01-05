84b31e94d96ff58b49abdb224bcb0f7242ab76dc
arbabali1441 <arbabali1441@gmail.com>
2021-01-06 02:34:23 +0500

    Show sidebar on homepage 20:55

---
+++ src/components/Telegram.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Sidebar from './Sidebar';
+
+
+function Telegram() {
+  return (
+    //BEM !!!!
+    <div className="telgram">
+      <Sidebar />
+    </div>
+  )
+}
+
+export default Telegram;
